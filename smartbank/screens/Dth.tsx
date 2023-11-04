20346f5aa35b5438b5a1e3d522f52ec15e3bfb37
sonarosa <sonarosababloo@gmail.com>
2023-11-04 18:53:22 +0530

    newupdates

--- smartbank/screens/Dth.tsx
+++ smartbank/screens/Dth.tsx
@@ -1,42 +1,74 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity, TextInput } from 'react-native';
 
 const DTHOperators = [
-  { name: 'Dish TV',},
-  { name: 'Tata Sky', },
-  { name: 'Airtel Digital TV', },
+  { name: 'Dish TV' },
+  { name: 'Tata Sky' },
+  { name: 'Airtel Digital TV' },
+  { name: 'Videocon D2H' },
+  { name: 'Sun Direct' },
   // Add more DTH operators here
 ];
 
 const DTHRechargeScreen = () => {
   const [selectedOperator, setSelectedOperator] = useState(DTHOperators[0]);
+  const [rechargeAmount, setRechargeAmount] = useState('');
+
+  const handlePayment = () => {
+    // Add your payment logic here, e.g., display an alert with the recharge details
+    alert(`Recharge Amount: ${rechargeAmount}\nOperator: ${selectedOperator.name}`);
+  };
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24, marginBottom: 20 }}>Select DTH Operator</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {DTHOperators.map((operator, index) => (
+      <FlatList
+        data={DTHOperators}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
           <TouchableOpacity
-            key={index}
-            onPress={() => setSelectedOperator(operator)}
+            onPress={() => setSelectedOperator(item)}
             style={{
+              flexDirection: 'row',
               alignItems: 'center',
-              marginHorizontal: 10,
-              borderWidth: selectedOperator === operator ? 2 : 0,
-              borderColor: selectedOperator === operator ? 'blue' : 'transparent',
+              marginVertical: 10,
+              borderWidth: selectedOperator === item ? 2 : 0,
+              borderColor: selectedOperator === item ? 'red' : 'transparent',
               borderRadius: 10,
             }}
           >
-            <Image
-              source={operator.icon}
-              style={{ width: 100, height: 100 }}
-            />
-            <Text>{operator.name}</Text>
+            {/* Add the operator's icon here if you have the images */}
+            <Text style={{ marginLeft: 10 }}>{item.name}</Text>
           </TouchableOpacity>
-        ))}
-      </ScrollView>
+        )}
+      />
       <Text style={{ fontSize: 20, marginTop: 20 }}>Selected Operator: {selectedOperator.name}</Text>
-      {/* Add the recharge button and functionality here */}
+      <Text style={{ fontSize: 20, marginTop: 20 }}>Enter Recharge Amount:</Text>
+      <TextInput
+        style={{
+          fontSize: 18,
+          borderWidth: 1,
+          borderColor: 'gray',
+          borderRadius: 10,
+          padding: 10,
+          width: 200,
+        }}
+        value={rechargeAmount}
+        onChangeText={text => setRechargeAmount(text)}
+      />
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'red',
+          padding: 10,
+          borderRadius: 10,
+          marginTop: 20,
+        }}
+        onPress={() => handlePayment()}
+      >
+        <Text style={{ fontSize: 18, color: 'white' }}>Pay</Text>
+      </TouchableOpacity>
     </View>
   );
 };
